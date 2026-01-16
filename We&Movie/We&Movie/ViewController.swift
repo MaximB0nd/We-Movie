63590dea7fd68b3f7add66b2975e60d36656ca39
MaximB0nd <sersim11@yandex.ru>
2026-01-16 23:25:17 +0300

    init: init base UIKit project

---
+++ We&Movie/We&Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  We&Movie
+//
+//  Created by Максим Бондарев on 16/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
