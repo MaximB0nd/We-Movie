c70559c7b435dac17fbc0f501a877989f36a7e6a
MaximB0nd <sersim11@yandex.ru>
2026-02-09 11:52:03 +0300

    feat: add custom changing app theme

--- We&Movie/We&Movie/Views/BaseVC.swift
+++ We&Movie/We&Movie/Views/BaseVC.swift
@@ -13,8 +13,10 @@ class BaseVC: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupConstraints()
+        setupTraitChangeObservation()
+        handleThemeChangeIfNeeded(previousStyle: nil)
     }
-    
+
     func setupUI() {
         view.backgroundColor = .systemBackground
         // Переопределить в дочерних классах
@@ -23,10 +25,6 @@ class BaseVC: UIViewController {
     func setupConstraints() {
         // Переопределить в дочерних классах
     }
-    
-    func updateColorsForCurrentTheme() {
-        
-    }
 
     func showError(_ error: Error) {
         AlertProvider.shared.show(error: error, on: self)
@@ -34,5 +32,43 @@ class BaseVC: UIViewController {
 
     func showMessage(_ message: String, title: String = "Ошибка") {
         AlertProvider.shared.show(message: message, title: title, on: self)
+    }
+
+    // Переопределить в дочерних классах, если нужно отслеживать другие трейты
+    func setupTraitChangeObservation() {
+        registerForTraitChanges([UITraitUserInterfaceStyle.self]) {
+            [weak self] (traitEnvironment: Self, previousTraits: UITraitCollection) in
+
+            let previousStyle = previousTraits.userInterfaceStyle
+            let currentStyle = traitEnvironment.traitCollection.userInterfaceStyle
+
+            // Проверяем, изменилась ли тема
+            if previousStyle != currentStyle {
+               self?.handleThemeChangeIfNeeded(previousStyle: previousStyle)
+            }
+        }
+    }
+    
+    // Переопределить в дочерних классах для светлой темы
+    func applyLightTheme() {
+        view.backgroundColor = .accentWhite
+    }
+
+    // Переопределить в дочерних классах для темной темы
+    func applyDarkTheme() {
+        view.backgroundColor = .accentBlue
+    }
+
+    private func handleThemeChangeIfNeeded(previousStyle: UIUserInterfaceStyle?) {
+        let currentStyle = traitCollection.userInterfaceStyle
+        if let previousStyle, previousStyle == currentStyle {
+            return
+        }
+        switch currentStyle {
+        case .dark:
+            applyDarkTheme()
+        default:
+            applyLightTheme()
+        }
     }
 }
