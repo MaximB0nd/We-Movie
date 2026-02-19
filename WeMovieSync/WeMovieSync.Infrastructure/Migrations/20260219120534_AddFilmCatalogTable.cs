5bb29531ce83e2470134c53e0b20b40998d943ab
sqtwix <ivan20140767@gmail.com>
2026-02-19 15:20:36 +0300

    feat: film catalog model

---
+++ WeMovieSync/WeMovieSync.Infrastructure/Migrations/20260219120534_AddFilmCatalogTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WeMovieSync.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilmCatalogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FilmCatalog",
+                columns: table => new
+                {
+                    Token = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FilmName = table.Column<string>(type: "text", nullable: false),
+                    FilmDescription = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<byte[]>(type: "bytea", nullable: true),
+                    Category = table.Column<string>(type: "text", nullable: true),
+                    Duration = table.Column<TimeSpan>(type: "interval", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilmCatalog", x => x.Token);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FilmCatalog_Token",
+                table: "FilmCatalog",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FilmCatalog");
+        }
+    }
+}
